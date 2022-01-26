067435b8b9c89a1d98c440b23182563fe9f759d7
Jan Wojdylak <jrwojdylak@gmail.com>
2022-01-26 19:47:23 +0100

    initial commit

---
+++ src/controllers/MainWindowController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class MainWindowController {
+
+
+    public MainWindowController() {
+    }
+
+    @FXML
+    void initialize(){
+    }
+
+}
