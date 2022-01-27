c30e277b12de346811bd9ead9f7a008410b3cc3a
Jan Wojdylak <jrwojdylak@gmail.com>
2022-01-27 12:27:09 +0100

    Update insert client to db

--- src/repositories/ClientRepository.java
+++ src/repositories/ClientRepository.java
@@ -3,27 +3,21 @@ package repositories;
 import database.DataBaseManager;
 import models.*;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 public class ClientRepository implements Repository<Client> {
-    private Statement query;
-    public ClientRepository() {
-        try{
-            this.query = DataBaseManager.connection.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 
 
     @Override
     public void save(Client client) throws SQLException {
-        String sql = "INSERT INTO biuro.Klient(imie, nazwisko) VALUES('" + client.getFirstName() + "', '" + client.getLastName() +"')";
-        query.executeUpdate(sql);
-//        System.out.println("Zapis do bazy klienta: " + client.getFirstName() + " " + client.getLastName());
+        PreparedStatement statement = DataBaseManager.connection.prepareStatement("INSERT INTO biuro.Klient(imie, nazwisko) VALUES(?,?)");
+        statement.setString(1,client.getFirstName());
+        statement.setString(2,client.getLastName());
+        statement.executeUpdate();
+        statement.close();
     }
 
     @Override
